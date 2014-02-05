c794e73d515c99b5fa5bf72fe95b2b08addbaced
Microcas <devnull@localhost>
2014-02-04 19:55:34 -0600

    Added GPLv3 headers.

--- TMP175.h
+++ TMP175.h
@@ -1,3 +1,24 @@
+/** 
+ *  TMP175.cpp
+ *  
+ *  This is a library for interfacing to the Texas Instruments TMP175 
+ *  temperature measurement IC.  
+ *
+ *  Copyright (C) 2014  Clint Stevenson (CascoLogix http://cascologix.com)
+ *
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
 
 
 #ifndef TMP175_h
